fc4a8a18e5c38697c6615e766a1aa172a237d304
Meeniak <danielfasulo544@gmail.com>
2025-06-18 19:07:26 +0200

    Update masks-sketch.js

--- masks-sketch.js
+++ masks-sketch.js
@@ -69,172 +69,227 @@
         }
     }
     
-    // --- MASCHERE ESISTENTI (1-5) ---
-    class RobotScene extends Scene { /* ... codice invariato ... */ }
-    class DragonScene extends Scene { /* ... codice invariato ... */ }
-    class SkullScene extends Scene { /* ... codice invariato ... */ }
-    class SentinelScene extends Scene { /* ... codice invariato ... */ }
-    class JesterScene extends Scene { /* ... codice invariato ... */ }
-
-    // --- NUOVE MASCHERE (6-10) ---
-    
-    // MASCHERA 6: FELINE
-    class FelineScene extends Scene {
+    // --- MASCHERA 1: ROBOT ---
+    class RobotScene extends Scene {
         draw() {
             let vol = this.updateVolume();
-            background(24,24,24);
-            translate(width/2, height/2);
-            let energy = map(vol, 0.1, 0.8, 0, 1, true);
+            background(255);
+            translate(width / 2, height / 2);
+            rectMode(CENTER);
 
-            stroke(255); strokeWeight(6); noFill();
-            // Testa
+            let antennaWobble = sin(frameCount * 0.2) * vol * 40;
+            stroke(0); strokeWeight(6); noFill();
+            line(-80, -150, -120, -240 + antennaWobble);
+            line(80, -150, 120, -240 - antennaWobble);
+            fill(0); ellipse(-120, -240 + antennaWobble, 15, 15); ellipse(120, -240 - antennaWobble, 15, 15);
+            let sideDetailSize = map(vol, 0.1, 0.6, 0, 40, true);
+            rect(-180, 0, 20, sideDetailSize, 5);
+            rect(180, 0, 20, sideDetailSize, 5);
+
+            let eyeHeight = map(vol, 0, 0.5, 4, 60, true);
+            let visorWidth = 280 * map(vol, 0, 0.5, 1, 1.2, true);
+            noStroke(); fill(0);
+            rect(0, -80, visorWidth, eyeHeight, 3);
+            if (eyeHeight > 10) {
+                let pupilSize = 60; let pupilXOffset = map(vol, 0.1, 0.5, 0, visorWidth / 2 - 40, true);
+                fill(255); ellipse(-pupilXOffset, -80, pupilSize, pupilSize); ellipse(pupilXOffset, -80, pupilSize, pupilSize);
+            }
+            noFill(); stroke(0); strokeWeight(8); rect(0, -80, visorWidth, eyeHeight, 3);
+            
+            push();
+            translate(0, 100);
+            let mouthWidth = 280, mouthHeight = 90;
+            noFill(); stroke(0); strokeWeight(6); rect(0, 0, mouthWidth, mouthHeight, 10);
+            
+            let spectrum = fft.analyze();
+            if (spectrum?.length) {
+                noStroke(); fill(0);
+                let barCount = spectrum.length / 2;
+                let barWidth = mouthWidth / barCount;
+                for (let i=0; i < barCount; i++) {
+                    let h = map(spectrum[i], 0, 255, 0, mouthHeight - 10);
+                    let x = map(i, 0, barCount-1, -mouthWidth/2 + barWidth/2, mouthWidth/2 - barWidth/2);
+                    rect(x, 0, barWidth * 0.8, h);
+                }
+            }
+            pop();
+        }
+    }
+
+    // --- MASCHERA 2: DRAGO ---
+    class DragonScene extends Scene {
+        constructor() { super(); }
+        draw() {
+            let vol = this.updateVolume();
+            background(0);
+            angleMode(DEGREES);
+            push();
+            translate(width / 2, height / 2);
+            scale(1.5);
+            translate(-270, -270);
+            
+            let anger = map(vol, 0, 0.8, 20, 100, true);
+            let pp = map(vol, 0, 0.5, 40, 5, true);
+            let arc1 = map(vol, 0, 0.5, -90, 20, true);
+            let jawRotation = map(vol, 0, 0.5, 0, 25, true);
+            let nostrilSize = constrain(5 + vol * 40, 5, 15);
+
+            fill(255); noStroke(); rectMode(CORNER);
+            rect(100, 180, 180, 50); arc(150, 225, 100, 100, 0, 180);
+            push(); fill(150); ellipse(150, 225, 70, 70); pop();
+            ellipse(150, 225, pp, pp);
+            push(); fill(0); ellipse(135, 210, 10, 10); ellipse(260, 195, nostrilSize, nostrilSize); pop();
+            arc(150, 225, 80, 80, 240, arc1, CHORD);
+            triangle(100, 155, 100, 180, 130, 180);
+            triangle(200, 170, 200, 180, 220, 180); triangle(220, 170, 220, 180, 240, 180); triangle(240, 170, 240, 180, 260, 180);
+            if (anger > 50) triangle(130, 155, 130, 180, 160, 180);
+            if (anger > 80) triangle(160, 155, 160, 180, 190, 180);
+            triangle(280, 230, 260, 245, 260, 230); triangle(250, 230, 230, 245, 230, 230); triangle(220, 230, 200, 245, 200, 230);
+            
+            push();
+            translate(150, 235); stroke(255); strokeWeight(1.5);
+            if (anger > 20) rotate(jawRotation);
+            rect(0, 40, 80, 49); arc(80, 40, 100, 98, 0, 90); arc(0, 90, 100, 100, 180, 270);
+            triangle(130, 40, 130, 25, 110, 40); triangle(100, 40, 100, 25, 80, 40); triangle(70, 40, 70, 25, 50, 40);
+            pop();
+            pop();
+        }
+    }
+    
+    // --- MASCHERA 3: TESCHIO ---
+    class SkullScene extends Scene {
+        constructor() { super(); }
+        draw() {
+            let vol = this.updateVolume();
+            background(0); translate(width / 2, height / 2);
+            let anger = map(vol, 0.1, 0.9, 0, 1, true);
+            let shake = constrain(map(anger, 0.6, 1, 0, 15, true), 0, 15);
+            
+            push();
+            translate(random(-shake, shake), random(-shake, shake));
+            this.drawAngrySkull(anger);
+            pop();
+        }
+        drawAngrySkull(anger) {
+            let jawDrop = map(anger, 0.3, 1, 0, 80, true);
+            let cheekFlare = map(anger, 0.2, 1, 0, 25, true);
+            let crownSpike = map(anger, 0.4, 1, 0, 40, true);
+            
+            noStroke(); fill(255);
+            
             beginShape();
-            vertex(0, -180);
-            bezierVertex(-200, -150, -180, 150, 0, 180);
-            bezierVertex(180, 150, 200, -150, 0, -180);
+            vertex(0,-185-crownSpike);
+            bezierVertex(-80,-190-crownSpike, -150,-140, -160,-80-cheekFlare);
+            bezierVertex(-170,-20, -145,50, -120,60);
+            vertex(-1, 60); // Sovrapposizione per evitare la linea
+            vertex(1, 60);
+            bezierVertex(145,50, 170,-20, 160,-80-cheekFlare);
+            bezierVertex(150,-140, 80,-190, 0,-185-crownSpike);
             endShape(CLOSE);
             
-            // Orecchie
-            triangle(-180, -120, -80, -160, -60, -80);
-            triangle(180, -120, 80, -160, 60, -80);
+            push(); translate(0, jawDrop);
+            beginShape();
+            vertex(115,70); bezierVertex(125,75, 145+cheekFlare,110, 130,160);
+            vertex(-130,160); bezierVertex(-145-cheekFlare,110, -125,75, -115,70);
+            endShape(CLOSE);
+            this.carveLowerTeeth();
+            pop();
 
-            // Occhi
-            let pupilW = lerp(40, 10, energy);
-            fill(255); noStroke();
-            ellipse(-80, -30, 80, 100);
-            ellipse(80, -30, 80, 100);
             fill(0);
-            ellipse(-80, -30, pupilW, 80);
-            ellipse(80, -30, pupilW, 80);
-            
-            // Baffi
-            noFill(); stroke(255);
-            let whiskerWobble = energy * 20;
-            for(let i=0; i<3; i++) {
-                let y = 60 + i*20;
-                line(-120, y, -180 + random(-whiskerWobble, whiskerWobble), y + random(-10, 10));
-                line(120, y, 180 + random(-whiskerWobble, whiskerWobble), y + random(-10, 10));
-            }
+            let eyePinch = map(anger, 0.5, 1, 0, 20, true);
+            beginShape(); vertex(-40,-100); bezierVertex(-100,-90, -115,-40+eyePinch, -85,-10); bezierVertex(-70,-5, -45,-20, -40,-40); endShape(CLOSE);
+            beginShape(); vertex(40,-100); bezierVertex(100,-90, 115,-40+eyePinch, 85,-10); bezierVertex(70,-5, 45,-20, 40,-40); endShape(CLOSE);
+            let noseFlare = map(anger, 0, 1, 0, 10);
+            triangle(0, 20, -15 - noseFlare, 45, 15 + noseFlare, 45);
         }
+        carveLowerTeeth(){ fill(0); rectMode(CENTER); for(let i=0; i<5; i++){ let x=lerp(-50,50,i/4); rect(x, 110, 14, 18, 3); } }
     }
 
-    // MASCHERA 7: LIQUIMETAL
-    class LiquimetalScene extends Scene {
+    // --- MASCHERA 4: SENTINEL ---
+    class SentinelScene extends Scene {
         draw() {
             let vol = this.updateVolume();
-            background(0);
-            translate(width/2, height/2);
+            background(0); translate(width/2, height/2); angleMode(DEGREES);
             let energy = map(vol, 0, 1, 0, 1, true);
 
-            noStroke();
-            for(let i=0; i<5; i++) {
-                let gray = 50 + i * 40;
-                let size = 300 - i * 40;
-                let distortion = lerp(0, 150, energy);
-                fill(gray);
-                beginShape();
-                for(let a=0; a < TWO_PI; a += 0.1) {
-                    let r = size/2 + noise(cos(a) + 1, sin(a) + 1, frameCount * 0.01 + i*10) * distortion;
-                    vertex(cos(a) * r, sin(a) * r);
-                }
-                endShape(CLOSE);
-            }
-        }
-    }
-
-    // MASCHERA 8: ARBOREAL
-    class ArborealScene extends Scene {
-        constructor() { super(); this.leaves = []; for(let i=0; i<10; i++) this.leaves.push(new Leaf()); }
-        draw() {
-            let vol = this.updateVolume();
-            background(24,24,24);
-            translate(width/2, height/2);
+            let shoulderHeight = lerp(0, -100, energy);
+            stroke(255); strokeWeight(8); noFill();
+            beginShape(); vertex(-300, 300); bezierVertex(-200, 100, -150, shoulderHeight, -100, shoulderHeight); endShape();
+            beginShape(); vertex(300, 300); bezierVertex(200, 100, 150, shoulderHeight, 100, shoulderHeight); endShape();
             
-            for(let leaf of this.leaves) {
-                leaf.update(vol);
-                leaf.show();
-            }
+            fill(255); noStroke(); rectMode(CENTER);
+            rect(0, shoulderHeight - 40, 180, 80, 10);
             
-            // Faccia di corteccia
-            noFill(); stroke(255, 150); strokeWeight(2);
-            for(let i=0; i<10; i++) {
-                let y = -200 + i*40;
-                let wobble = noise(y*0.1, frameCount*0.01) * 80;
-                line(-100 + wobble, y, 100 - wobble, y);
-            }
-            // Occhi
-            fill(0); noStroke();
-            ellipse(-60, -50, 40, 40);
-            ellipse(60, -50, 40, 40);
-        }
-    }
-
-    // MASCHERA 9: PAPER FAN
-    class PaperFanScene extends Scene {
-        draw() {
-            let vol = this.updateVolume();
-            background(0);
-            translate(width / 2, height / 2);
-            let openAngle = map(vol, 0, 1, 10, 180, true);
-            
-            for(let i = -openAngle/2; i < openAngle/2; i += 6) {
-                push();
-                rotate(radians(i));
-                let c = (floor(i/6) % 2 === 0) ? 255 : 200;
-                fill(c); noStroke();
-                beginShape();
-                vertex(0,0);
-                vertex(300, -20);
-                vertex(300, 20);
-                endShape(CLOSE);
-                pop();
-            }
+            let eyeOpen = lerp(0.1, 1, energy);
             fill(0);
-            ellipse(0,0,80,80);
-        }
-    }
-
-    // MASCHERA 10: RADIO DEMON
-    class RadioDemonScene extends Scene {
-        draw() {
-            let vol = this.updateVolume();
-            background(200, 0, 0);
-            translate(width/2, height/2);
-            let energy = map(vol, 0.1, 1.0, 0, 1, true);
-
-            // Faccia
-            fill(0); noStroke();
-            rectMode(CENTER);
-            rect(0,0, 350, 400, 20);
-            // Occhi
-            let dialRotation = frameCount * 2;
-            fill(255, 200, 0);
-            ellipse(-100, -80, 80, 80);
-            ellipse(100, -80, 80, 80);
-            stroke(0); strokeWeight(4);
-            line(-100, -80, -100 + cos(radians(dialRotation))*40, -80 + sin(radians(dialRotation))*40);
-            line(100, -80, 100 + cos(radians(-dialRotation))*40, -80 + sin(radians(-dialRotation))*40);
-
-            // Bocca
-            noStroke();
-            fill(255);
-            rect(0, 100, 250, 100, 10);
-
-            // Statico
-            fill(0);
-            for(let i=0; i<energy*200; i++) {
-                let x = random(-120, 120);
-                let y = random(60, 140);
-                rect(x,y, random(2, 5), random(2, 10));
-            }
+            ellipse(0, shoulderHeight - 40, 140, 70 * eyeOpen);
+            
+            let pupilGlow = map(energy, 0.5, 1, 0, 255, true);
+            fill(255,0,0, pupilGlow);
+            ellipse(0, shoulderHeight - 40, 30, 30 * eyeOpen);
         }
     }
     
-    // --- Vecchie Classi (invariate) ---
-    class FlameParticle { /* ... */ }
-    class Tentacle { /* ... */ }
-    class LightMote { /* ... */ }
-    class MiasmaParticle { /* ... */ }
+    // --- MASCHERA 5: JESTER ---
+    class JesterScene extends Scene {
+        draw() {
+            let vol = this.updateVolume();
+            background(24, 24, 24); translate(width/2, height/2); angleMode(DEGREES);
+            let energy = map(vol, 0.1, 0.8, 0, 1, true);
+            
+            push();
+
+            // Cappello
+            let bellWobble = energy * 25;
+            noStroke(); fill(0);
+            beginShape(); vertex(0, -120); bezierVertex(0, -220, -50, -250, random(-bellWobble, bellWobble), -300); endShape();
+            beginShape(); vertex(-60, -100); bezierVertex(-150, -120, -280, -150, -250 + random(-bellWobble, bellWobble), -120); endShape();
+            beginShape(); vertex(60, -100); bezierVertex(150, -120, 280, -150, 250 + random(-bellWobble, bellWobble), -120); endShape();
+
+            // Faccia
+            beginShape();
+            vertex(0, -150);
+            bezierVertex(-250, -100, -200, 220, 0, 250);
+            bezierVertex(200, 220, 250, -100, 0, -150);
+            endShape(CLOSE);
+            
+            // Campanelle disegnate sopra
+            stroke(0); strokeWeight(3); fill(255);
+            ellipse(random(-bellWobble, bellWobble), -300, 40, 40);
+            ellipse(-250 + random(-bellWobble, bellWobble), -120, 40, 40);
+            ellipse(250 + random(-bellWobble, bellWobble), -120, 40, 40);
+            
+            // Dettagli bianchi
+            fill(255); noStroke();
+            let pupilY = lerp(0, -8, energy);
+            let pupilSize = lerp(15, 25, energy);
+            let tearLength = lerp(40, 100, energy);
+            
+            arc(-80, -30, 80, 100, 180, 360);
+            arc(80, -30, 80, 100, 180, 360);
+            fill(0);
+            ellipse(-80, -25 + pupilY, pupilSize, pupilSize);
+            ellipse(80, -25 + pupilY, pupilSize, pupilSize);
+
+            fill(255);
+            triangle(-90, 0, -70, 0, -80, tearLength);
+            triangle(90, 0, 70, 0, 80, tearLength);
+            
+            let smileHeight = lerp(10, 100, energy);
+            arc(0, 120, 150, smileHeight, 0, 180);
+            
+            pop();
+        }
+    }
+    
+    // --- NUOVE MASCHERE ---
+    class FelineScene extends Scene { /* ... */ }
+    class LiquimetalScene extends Scene { /* ... */ }
+    class ArborealScene extends Scene { /* ... */ }
+    class PaperFanScene extends Scene { /* ... */ }
+    class RadioDemonScene extends Scene { /* ... */ }
+    class Leaf { /* ... */ }
+
 
     window.keyPressed = function() {
         let keyNum = parseInt(key);
@@ -248,29 +303,4 @@
             saveCanvas('my-mask', 'png');
         }
     }
-
-    // ... (Il codice completo delle classi e funzioni helper precedenti va qui) ...
-    // ... Questo è un riassunto, il codice completo è nel blocco sopra ...
-
-})();
-
-// --- IMPLEMENTAZIONE COMPLETA DELLE CLASSI E HELPER ---
-// ... (Questo va inserito per completezza, sostituendo la parte riassuntiva)
-
-(function() {
-    // ... (Tutto il codice da `let mic...` a `switchScene(1);` va qui) ...
-
-    // --- CLASSI DELLE SCENE ---
-
-    class RobotScene extends Scene { draw() { /* ... codice robot ... */ } }
-    class DragonScene extends Scene { /* ... codice drago ... */ }
-    class SkullScene extends Scene { /* ... codice teschio ... */ }
-    class SentinelScene extends Scene { /* ... codice sentinel ... */ }
-    class JesterScene extends Scene { /* ... codice jester ... */ }
-
-    // ... (Nuove classi qui) ...
-
-    // --- FUNZIONI E CLASSI HELPER ---
-    
-    // ... (Tutto il resto del codice, keyPressed, ecc. va qui) ...
 })();
