228b882c983538777746f4c6e147b2c6441cb555
Meeniak <danielfasulo544@gmail.com>
2025-06-26 05:54:18 +0200

    Update filters-sketch.js

--- filters-sketch.js
+++ filters-sketch.js
@@ -4,6 +4,9 @@
     let filterLabel;
     let currentFilter = 1;
 
+    // Buffer grafico per centrare l'immagine
+    let webcamBuffer; 
+
     const asciiChars = '       .:-i|=+*#%@';
 
     const filterNames = {
@@ -12,6 +15,7 @@
         3: 'Thermal Vision'
     };
 
+    // Funzione per aggiornare gli slider quando si cambia filtro
     function updateSliders() {
         switch(currentFilter) {
             case 1: // Mosaic
@@ -39,12 +43,14 @@
         webcam.size(width, height);
         webcam.hide();
 
+        webcamBuffer = createGraphics(width, height);
+
         filterLabel = select('#current-filter-label');
         mainSlider = createSlider(0,0,0,0);
         mainSlider.parent('slider-main-container');
         mainSlider.style('width', '100%');
 
-        paramLabel = select('#param-label');
+        paramLabel = select('#param-label'); // Seleziona l'etichetta tramite ID
         paramSlider = createSlider(0,0,0,0);
         paramSlider.parent('slider-param-container');
         paramSlider.style('width', '100%');
@@ -57,10 +63,17 @@
     }
 
     window.draw = function() {
+        // 1. Disegna la webcam specchiata nel buffer UNA SOLA VOLTA
+        webcamBuffer.push();
+        webcamBuffer.translate(width, 0);
+        webcamBuffer.scale(-1, 1);
+        webcamBuffer.image(webcam, 0, 0, width, height);
+        webcamBuffer.pop();
+
+        // 2. Applica il filtro scelto
         let mainValue = mainSlider.value();
         let paramValue = paramSlider.value();
         
-        // Ogni filtro gestisce il proprio sfondo e la propria immagine
         switch (currentFilter) {
             case 1:
                 drawMosaicFilter(mainValue, paramValue);
@@ -74,27 +87,16 @@
         }
     }
 
-    function drawMirroredWebcam() {
-        push();
-        translate(width, 0);
-        scale(-1, 1);
-        image(webcam, 0, 0, width, height);
-        pop();
-    }
-
     function drawMosaicFilter(cellSize, angle) {
-        background(0);
-        // La webcam non viene mostrata direttamente, ma usata per i tasselli
+        // Disegna prima il video di base per vederlo
+        image(webcamBuffer, 0, 0, width, height);
+        // Poi applica i tasselli ruotati sopra
         for (let x = 0; x < width; x += cellSize) {
             for (let y = 0; y < height; y += cellSize) {
                 push();
                 translate(x + cellSize / 2, y + cellSize / 2);
                 rotate(angle);
-                // Specchia il singolo tassello per un effetto specchio corretto
-                push();
-                scale(-1,1);
-                image(webcam, -cellSize / 2, -cellSize / 2, cellSize, cellSize, width - x - cellSize, y, cellSize, cellSize);
-                pop();
+                image(webcamBuffer, -cellSize / 2, -cellSize / 2, cellSize, cellSize, x, y, cellSize, cellSize);
                 pop();
             }
         }
@@ -102,22 +104,17 @@
 
     function drawAsciiFilter(cellSize) {
         background(0);
-        let mirroredCam = webcam.get();
-        mirroredCam.loadPixels();
-        
+        webcamBuffer.loadPixels();
         fill(255, 220);
         noStroke();
         textSize(cellSize * 1.2);
 
         for (let y = 0; y < height; y += cellSize) {
             for (let x = 0; x < width; x += cellSize) {
-                // Indice per l'immagine specchiata
-                let mirroredX = width - x - 1;
-                let pixelIndex = (mirroredX + y * width) * 4;
-                
-                let r = mirroredCam.pixels[pixelIndex];
-                let g = mirroredCam.pixels[pixelIndex + 1];
-                let b = mirroredCam.pixels[pixelIndex + 2];
+                let pixelIndex = (x + y * width) * 4;
+                let r = webcamBuffer.pixels[pixelIndex];
+                let g = webcamBuffer.pixels[pixelIndex + 1];
+                let b = webcamBuffer.pixels[pixelIndex + 2];
                 let brightness = (r + g + b) / 3;
                 
                 let charIndex = floor(map(brightness, 0, 255, 0, asciiChars.length - 1));
@@ -129,14 +126,13 @@
     function drawThermalFilter(cellSize) {
         background(0);
         noStroke();
-        webcam.loadPixels();
+        webcamBuffer.loadPixels();
         for (let x = 0; x < width; x += cellSize) {
             for (let y = 0; y < height; y += cellSize) {
-                let mirroredX = width - x - 1;
-                let index = (mirroredX + y * width) * 4;
-                let r = webcam.pixels[index];
-                let g = webcam.pixels[index + 1];
-                let b = webcam.pixels[index + 2];
+                let index = (x + y * width) * 4;
+                let r = webcamBuffer.pixels[index];
+                let g = webcamBuffer.pixels[index + 1];
+                let b = webcamBuffer.pixels[index + 2];
                 let brightness = (r + g + b) / 3;
                 
                 let thermalColor = getColorForBrightness(brightness);
@@ -167,16 +163,3 @@
         }
     }
 })();
-
-
-
-
-
-
-
-
-
-
-
-
-
